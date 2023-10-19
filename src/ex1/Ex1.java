7f215b90bc3499469ef824ee0bee081b1b0aa5a5
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-19 14:22:33 -0300

    ajustes do Ex1

--- src/ex1/Ex1.java
+++ src/ex1/Ex1.java
@@ -13,30 +13,27 @@ public class Ex1 {
         System.out.println("Digite seu nome: ");
         nome = leitura.nextLine();
         while (nome.length() < 2 || nome.length() > 30){
-            System.out.println("numero de caracteres invalido");
-            System.out.println("digite entre 2 e 30 caracteres.");
+            System.out.println("Número de caracteres inválido");
+            System.out.println("Digite entre 2 e 30 caracteres.");
             nome = leitura.nextLine();
         }
-
 
         System.out.println("Digite sua idade (0,99): ");
         idade = leitura.nextInt();
         while (idade < 0 || idade > 99) {
-            System.out.println("idade invalida");
+            System.out.println("Idade inválida.");
             System.out.println("Digite sua idade (0 a 99): ");
             idade = leitura.nextInt();
-
         }
 
-
-        System.out.println("selecione a opção de gênero desejada: ");
-        System.out.println("1 Para masculino, 2 para feminino ");
+        System.out.println("Selecione a opção de gênero desejada: ");
+        System.out.println("1 para masculino, 2 para feminino.");
         genero = leitura.nextInt();
 
         System.out.println("****************************************");
         System.out.println("Nome do cliente: " + nome);
-        System.out.println("idade do cliente: " + idade);
-        System.out.println("Genero do cliete: " + genero);
+        System.out.println("Idade do cliente: " + idade);
+        System.out.println("Gênero do cliente: " + genero);
         System.out.println("****************************************");
 
         if (idade < 18) {
