cff5f6a0d0b8141d0c5a908269e13e7184008774
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-20 08:44:04 -0300

    exercicio do Ex04

---
+++ src/dio/Ex04.java
@@ -0,0 +1,20 @@
+/*
+4 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
+
+ */
+
+package dio;
+
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero = teclado.nextInt();
+        int antecessor = numero - 1;
+        System.out.println(antecessor);
+        int sucessor = numero + 1;
+        System.out.println(sucessor);
+    }
+}
