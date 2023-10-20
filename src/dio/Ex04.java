aa7bed89824b05c362c959e3c14daeaa8538e945
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-20 08:49:17 -0300

    exercicio do Ex04

--- src/dio/Ex04.java
+++ src/dio/Ex04.java
@@ -10,10 +10,12 @@ import java.util.Scanner;
 public class Ex04 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Digite um número: ");
+        System.out.print("Digite um número:");
         int numero = teclado.nextInt();
+
         int antecessor = numero - 1;
         System.out.println(antecessor);
+
         int sucessor = numero + 1;
         System.out.println(sucessor);
     }
