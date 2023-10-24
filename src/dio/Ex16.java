24c3830ce02b1939c7045b10de12e2a41e401aa2
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-24 11:17:52 -0300

    exercicio Ex 16_1 melhorias

--- src/dio/Ex16.java
+++ src/dio/Ex16.java
@@ -1,19 +1,32 @@
 package dio;
 /*
-16. Faça um programa que leia um número inteiro digitado pelo usuário e informe na tela se o número é negativo (menor do que zero) ou se é positivo (maior ou igual a zero).
+16.1. Faça um programa que leia um número inteiro digitado pelo usuário e informe na tela se o número é negativo (menor do que zero) ou se é positivo (maior ou igual a zero).
+
+Em seguida, imprima na tela o valor absoluto do número (ou seja, o próprio número, mas sempre positivo).
+
+Exemplos:
+- Se o usuário informou o número 5, imprima: "O valor absoluto do número 5 é 5."
+- Se o usuário informou o número 0, imprima: "O valor absoluto do número 0 é 0."
+- Se o usuário informou o número -3, imprima: "O valor absoluto do número -3 é 3."
+- Se o usuário informou o número -1, imprima: "O valor absoluto do número -1 é 1."
  */
 
 import java.util.Scanner;
+
 public class Ex16 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite um número: ");
         int numero = teclado.nextInt();
+        int numeroAbsoluto;
 
         if (numero < 0) {
+            numeroAbsoluto = -numero;
             System.out.println("Número negativo");
         } else {
+            numeroAbsoluto = numero;
             System.out.println("Número positivo");
         }
+        System.out.println("O valor absoluto número " + numero + " é " + numeroAbsoluto + ".");
     }
 }
