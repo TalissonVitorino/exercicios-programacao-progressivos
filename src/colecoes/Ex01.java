76848e4cbb3553f37f0f1dc1e585f6715d1a7886
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-26 14:16:21 -0300

    exercicio Ex01 adicionado ate 4 pessoas

--- src/colecoes/Ex01.java
+++ src/colecoes/Ex01.java
@@ -1,9 +1,15 @@
 /*
-1. Leia o nome de uma pessoa e imprima na tela o nome e a quantidade de caracteres do nome.
-Exemplo:
+Alterar o programa do Exercício 1 para passar trabalhar com três nomes, em vez de um.
 
-Digite seu nome: Lucas
+O programa deve ler os nomes de três pessoas e imprimir tela o nome e a quantidade de caracteres de cada nome.
+
+Exemplo:
+Digite o nome da pessoa 1: Lucas
+Digite o nome da pessoa 2: Talisson
+Digite o nome da pessoa 3: José da Silva
 Lucas, 5 caracteres.
+Talisson, 8 caracteres.
+José da Silva, 13 caracteres.
  */
 
 package colecoes;
@@ -13,9 +19,23 @@ import java.util.Scanner;
 public class Ex01 {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+
         System.out.println("Digite seu nome: ");
         String nome = leitura.nextLine();
-        System.out.println("Nome do usuario: " +nome);
+
+        System.out.println("Digite seu nome: ");
+        String segundo = leitura.nextLine();
+
+        System.out.println("Digite seu nome: ");
+        String terceiro = leitura.nextLine();
+
+        System.out.println("Nome do usuario: " + nome);
+        System.out.println("Nome do usuario: " + segundo);
+        System.out.println("Nome do usuario: " + terceiro);
+        System.out.println();
+
         System.out.println(nome + "," + nome.length() + " caractere");
+        System.out.println(segundo + "," + segundo.length() + " caractere");
+        System.out.println(terceiro + "," + terceiro.length() + " caractere");
     }
 }
