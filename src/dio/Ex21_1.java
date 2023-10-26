c400736b0dd1e20ebb97ace8e23c97648799bebb
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-26 10:22:30 -0300

    exercicio Ex 21 calculadora

---
+++ src/dio/Ex21_1.java
@@ -0,0 +1,37 @@
+/*
+21.1. Faça uma calculadora de soma de números pares. Para isso,
+leia números digitados pelo usuário e depois mostre a soma de todos os números pares digitados.
+
+Enquanto o usuário estiver digitando números maiores do que zero, vá somando os números.
+ Mas quando o usuário digitar 0, interrompa a leitura e imprima na tela o valor da soma de todos os números pares digitados.
+
+Exemplo:
+Digite um número: 1
+Digite um número: 2
+Digite um número: 3
+Digite um número: 4
+Digite um número: 5
+Digite um número: 0
+
+A soma dos pares é: 6
+ */
+package dio;
+
+import java.util.Scanner;
+public class Ex21_1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int somado = 0;
+        int numero = 0;
+
+        do {
+            if (numero % 2 == 0){
+                somado = numero + somado;
+            }
+            System.out.print("Digite o valor a ser somado: ");
+            numero = teclado.nextInt();
+        } while (numero > 0);
+        System.out.println("O valor da soma de todos os números pares digitados " + somado + ".");
+    }
+}
