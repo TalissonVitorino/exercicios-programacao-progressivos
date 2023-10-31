26fcc776bfa1609c7485757737cb236a7fce2b6e
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-10-31 12:47:31 -0300

    exercicio Ex2 inicio

--- src/colecoes/Ex2.java
+++ src/colecoes/Ex2.java
@@ -1,37 +1,28 @@
 /*
-2. Faça um programa que leia números inteiros digitados pelo usuário e vá armazenando em uma lista. Vá lendo os números até que o usuário digite o número 0, o que deve interromper a leitura.
+Adicione, ao programa do exercício anterior, uma funcionalidade nova para percorrer a lista de números armazenados e,
+para cada número, imprimir se ele é par ou ímpar.
 
-Em seguida, percorra a lista, some todos os números e imprima o resultado da soma na tela.
+Por exemplo, se o usuário informar os números 2, 5 e 7, o programa deve imprimir:
 
-Dicas:
-
-Para declarar / criar uma lista de números inteiros:
-ArrayList<Integer> lista = new ArrayList<>();
-
-Para adicionar números inteiros na lista: igual ao que foi feito na lista de nomes do exercício anterior.
-
-Para percorrer a lista: igual ao que foi feito na lista de nomes do exercício anterior.
-
-Para ler números inteiros do teclado: igual ao que foi feito na lista de nomes do exercício antetior.
+2 é par.
+5 é ímpar.
+7 é ímpar.
  */
-
-
 
 package colecoes;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Ex2 {
     public static void main(String[] args) {
         ArrayList<Integer> lista = new ArrayList<>();
         Scanner leitura = new Scanner(System.in);
-        System.out.print("Digite um numero :");
+        System.out.print("Digite um número :");
         int numero = Integer.parseInt(leitura.nextLine());
 
         while (numero != 0) {
             lista.add(numero);
-            System.out.print("Digite um numero :");
+            System.out.print("Digite um número :");
             numero = Integer.parseInt(leitura.nextLine());
         }
 
@@ -39,8 +30,16 @@ public class Ex2 {
         for (Integer num : lista) {
             numerosSomados = numerosSomados + num;
         }
-        System.out.println("total de numeros somados é " + numerosSomados + ".");
+        System.out.println("total de números somados é " + numerosSomados + ".");
         double media = (double) numerosSomados / lista.size();
-        System.out.print("A média dos números é " + media + ".");
+        System.out.println("A média dos números é " + media + ".");
+
+        for (Integer num : lista) {
+            if (num % 2 == 0) {
+                System.out.println("Número par! " + num);
+            } else {
+                System.out.println("Número ímpar! " + num);
+            }
+        }
     }
 }
