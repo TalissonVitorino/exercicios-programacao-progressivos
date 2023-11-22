177f9010c3ce57b8c73f69e4bda33a7bf0499fe4
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-11-22 13:43:36 -0300

    Exercício Ex03 calculando media

---
+++ src/Lista1/Ex04.java
@@ -0,0 +1,20 @@
+package Lista1;
+
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int x = teclado.nextInt();
+
+        System.out.println("Digite outro numero: ");
+        int y = teclado.nextInt();
+
+        int quociente = x / y;
+        int restoDivisao = x - quociente * y;
+        System.out.println(quociente);
+        System.out.println(restoDivisao);
+    }
+}
