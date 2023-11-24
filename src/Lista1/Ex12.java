577a79fcaa96ebcd7651f6d290a42ba331e0aeec
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-11-24 14:39:14 -0300

    Exercício 11

---
+++ src/Lista1/Ex12.java
@@ -0,0 +1,32 @@
+package Lista1;
+
+import java.util.Scanner;
+
+public class Ex12 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Qual era a velocidade do motorista? ");
+        double velocidadeMotoriusta = teclado.nextDouble();
+        System.out.print("Qual era a velocidade maxima da pista? ");
+        double velocidadePista = teclado.nextDouble();
+
+        double excessoVelocidade = velocidadeMotoriusta - velocidadePista;
+
+        if (excessoVelocidade <= 0) {
+            System.out.println("Não tem multa.");
+        } else {
+            if (excessoVelocidade > 0 && excessoVelocidade <= 10) {
+                System.out.println("A multa sera 50. ");
+
+            } else {
+                if (excessoVelocidade > 10 && excessoVelocidade <= 30) {
+                    System.out.println("A multa sera 100. ");
+
+                } else {
+                    System.out.println("A multa sera 200. ");
+                }
+            }
+        }
+
+    }
+}
