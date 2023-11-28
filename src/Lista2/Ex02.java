b2d3d08d55f912ed5d535c2ba4fae505dde89772
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-11-28 11:21:38 -0300

    Exercício 2

---
+++ src/Lista2/Ex02.java
@@ -0,0 +1,19 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Imprimir os múltiplos de ");
+        int multiplicacao = teclado.nextInt();
+
+        System.out.print("menores ou iguais a ");
+        int limitador = teclado.nextInt();
+
+        for (int cont = multiplicacao; cont <= limitador; cont = cont + multiplicacao) {
+            System.out.println(""+ cont);
+        }
+    }
+}
